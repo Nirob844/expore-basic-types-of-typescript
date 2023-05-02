754f0ac4b43433174c4859642895d7a8fa630035
Nirob Hasan <nirob427417@gmail.com>
2023-05-02 16:42:36 +0600

    spread rest orerator

--- src/function.ts
+++ src/function.ts
@@ -5,3 +5,18 @@ function add(num1: number, num2: number): number {
 const addArrow = (num1: number, num2: number): number => num1 + num2;
 
 add(2, 3)
+
+
+//spread operator
+const myFriends = ['a', 'b', 'c']
+const newFriends = ['d', 'e']
+
+myFriends.push(...newFriends)
+console.log(myFriends);
+
+
+//rest operator
+const greetFriends = (...friends: string[]): void =>
+    friends.forEach((friend) => console.log(`hi ${friend}`));
+
+greetFriends('abul', 'kabul', 'babul')
